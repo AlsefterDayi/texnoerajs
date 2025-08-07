d0909345b3fc7d46096de6fba9b48f0b1a6b8c94
Huseyn <elsefter.dayi@gmail.com>
2025-08-07 11:59:03 +0400

    reqemli romb

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,17 @@ let table = document.querySelector(".table");
 const showTable = (rows, cols) => {
   table.innerHTML = "";
   let td = "";
+  const center = Math.floor(rows / 2);
   for (let i = 0; i < rows; i++) {
     let tr = document.createElement("tr");
-
     for (let j = 0; j < cols; j++) {
+      const differ = j - i + 1;
+      const differ2 = cols - i + 1;
+
       if ((j >= i && j < rows - i) || (j >= rows - 1 - i && j <= i)) {
-        td = `<td class="green"></td>`;
+        let distance = Math.abs(center - j);
+        td.textContent = center - distance + 1;
+        td = `<td class="green">${center - distance + 1}</td>`;
       } else {
         td = `<td></td>`;
       }
@@ -17,4 +22,4 @@ const showTable = (rows, cols) => {
     table.innerHTML += tr.outerHTML;
   }
 };
-showTable(15, 15);
+showTable(9, 9);
