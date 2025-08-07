7b9fe341ae60fa25dd480c5c00d5d564b8b5b661
Huseyn <elsefter.dayi@gmail.com>
2025-08-07 09:53:51 +0400

    table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,20 @@
-let addBtn = document.getElementById("adBtn");
-let addInput = document.getElementById("add");
-let list = document.getElementById("list");
-let obTasks = [];
-let tasks = fetch("https://jsonplaceholder.typicode.com/todos");
-tasks
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((task) => {
-      obTasks.push({
-        id: task.id,
-        title: task.title,
-        completed: task.completed,
-      });
-    });
-    showlist();
-  });
+let table = document.querySelector(".table");
+console.log("Table element:", table);
 
-function add() {
-  if (addInput.value === "") {
-    alert("Please enter a task");
-    return;
-  }
-  let newtask = addInput.value;
-  obTasks.push({
-    id: obTasks.length + 1,
-    title: newtask,
-    completed: false,
-  });
-  showlist();
-}
-function showlist() {
-  list.innerHTML = "";
-  obTasks.forEach((task) => {
-    let li = document.createElement("li");
-    li.innerHTML = `<span class="title">${task.title}</span>
-                        <button class="btn delete" onclick="deleteTask(${task.id})">Delete</button>`;
-    li.className = "list-item";
-    if (task.completed) {
-      li.classList.add("completed");
-    } else {
-      li.classList.remove("completed");
+const showTable = (rows, cols) => {
+  table.innerHTML = "";
+  let td = "";
+  for (let i = 0; i < rows; i++) {
+    let tr = document.createElement("tr");
+    for (let j = 0; j < cols; j++) {
+      if (i % 2 === 0) {
+        td = `<td>*</td>`;
+      } else {
+        td = `<td></td>`;
+      }
+      tr.innerHTML += td;
     }
-    list.appendChild(li);
-  });
-  addInput.value = "";
-}
-
-addBtn.addEventListener("click", add);
+    table.innerHTML += tr.outerHTML;
+  }
+};
+showTable(5, 5);
